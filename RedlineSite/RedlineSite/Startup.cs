f30aa607f0771581273a724a1804cfc59a19bc6d
george-mintoft1 <p17189448@my365.dmu.ac.uk>
2019-02-22 17:14:03 +0000

    initial7

---
+++ RedlineSite/RedlineSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RedlineSite.Startup))]
+namespace RedlineSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
